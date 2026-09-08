91508cdd8e00260953af9ab2e7e711648d982f84
Aryan Patel <bontaromiii@gmail.com>
2026-09-08 18:29:45 +0530

    added index.js for javascript watching a tutorial on javascript i wanted that when i hold click image cycles between a set of images, cause js is new to me took gpt help but i understood the code well as i know gdscript already,it works by an array of image paths that are beng changed by click on the image that adds +1 to a variable current that accounts for the osition on the array of patch that is then set as source of image

---
+++ index.js
@@ -0,0 +1,9 @@
+const image = document.getElementById("myimage")
+const images = ["images/me1",
+    "images/me1.jpg",
+    "images/me2.png",
+    "images/me3.png"
+];
+let current = 0;
+let interval;
+image.addEventListener("click",() => {current = (current+1)% images.length;image.src = images[current]});
