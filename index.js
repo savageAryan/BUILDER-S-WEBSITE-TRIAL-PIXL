a3c69218bcae35c86563eafafa40b86aa6823960
Aryan Patel <bontaromiii@gmail.com>
2026-09-10 20:15:53 +0530

    added more songs and with helpmof tutorials and some gpt made a function that picks a random song path from an array of paths of song and plays it as the source of music which is and audio elements id,i learnt to make it a random pick, called that func at start and end of a song,also tried to make the audiobutton work but will do it later it will pause and play the songs, since its my first time doing all this it takes a lot of time but I am liking it

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const image = document.getElementById("myimage")
-const images = ["images/me1",
+const images = [
     "images/me1.jpg",
     "images/me2.png",
     "images/me3.png"
@@ -8,4 +8,26 @@ let current = 0;
 let interval;
 image.addEventListener("click",() => {current = (current+1)% images.length;image.src = images[current]});
 const music = document.getElementById("music")
-document.addEventListener("click",() => {music.play();},{once: true});
+const musics = ["audios/song1.mp3",
+    "audios/song2.mp3",
+    "audios/song3.mp3",
+    "audios/song4.mp3",
+    
+]
+let currentsong = 1;
+let songinterval;
+function playrandomsong(){
+    const randommusic = musics[Math.floor(Math.random() * musics.length)];
+    music.src = randommusic;
+    music.play();
+}
+playrandomsong();
+document.addEventListener("click",() =>{
+    playrandomsong();
+    },{once: true});
+music.addEventListener("ended",() =>{
+    playrandomsong();
+});
+const audiobutton = document.getElementById("audiobutton")
+audiobutton.addEventListener("click",() => music.pause);
+
